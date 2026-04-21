4f7c728e5d92d75c6a32cb6fcc951c9318cf67d8
arkedinfotech2022-jpg <arkedinfotech2022@gmail.com>
2026-04-21 17:35:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const { PeerServer } = require('peer');
+
 const PORT = process.env.PORT || 3000;
 
-PeerServer({ port: PORT, path: '/peerjs', key: 'seestatus' });
-console.log(`PeerJS running on port ${PORT}`);
+const server = PeerServer({
+  port: PORT,
+  path: '/peerjs',
+  key: 'seestatus'
+});
+
+console.log(`PeerJS server running on port ${PORT}`);
+console.log(`Path: /peerjs`);
+console.log(`Key: seestatus`);
