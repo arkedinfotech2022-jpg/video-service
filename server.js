56623e72cf98060c78631d18c86531110622e1a3
arkedinfotech2022-jpg <arkedinfotech2022@gmail.com>
2026-04-21 17:39:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,17 @@ const { PeerServer } = require('peer');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Create PeerJS server
+// Create PeerJS server - attaches to the Express app
 const peerServer = PeerServer({
   port: PORT,
   path: '/peerjs',
   key: 'seestatus'
 });
 
-// Add a test route
+// Test route
 app.get('/', (req, res) => {
   res.send('PeerJS Server is running!');
 });
 
-app.get('/peerjs', (req, res) => {
-  res.send('PeerJS endpoint is active');
-});
-
 console.log(`Server running on port ${PORT}`);
+console.log(`PeerJS available at: /peerjs`);
